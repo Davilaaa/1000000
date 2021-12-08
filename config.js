f3b1cb4d1695238f19fce64c5d9270fe5cb78cc7
Davilaaa <95749907+Davilaaa@users.noreply.github.com>
2021-12-08 18:32:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi bạn Yêu Của tớ 🥰 ",
-    introTitle: 'Thúy Hằng ',
+    titleWeb: "Gửi Bé Yêu của Anh 🥰 ",
+    introTitle: 'Chi Nguyễn ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^Ấn Vào Đây^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tớ <3',
-    btnReply: 'Gửi cho cậu <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'tớ biết mà 🥰. Yêu  cậu nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 22h, chơi game với tớ nha 😘.',
+    btnReply: 'Gửi cho Em <3',
+    reply: 'Không Yêu Cũng Phải Yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu  Em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 22h, Anh sẽ gọi cho bé 😘.',
     btnAccept: 'Okiiiii lun !<3',
     messLink: 'https://www.facebook.com/du.om.5074/'
+}
